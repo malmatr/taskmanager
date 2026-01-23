246a1edd998eaf7b40606e4f937350f72279a178
mallory matravers <matraversmallory@gmail.com>
2026-01-22 18:13:18 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const connectDB = require("./db/connect");
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+require("dotenv").config();
+
+// middleware
+app.use(express.static("./public"));
+app.use(express.json());
+
+// routes
+
+app.use("/api/v1/tasks", tasks);
+
+// app.get('/api//v1/tasks')        - get all the tasks
+// app.post('/api/v1/tasks')        - create a new task
+// app.get('/api/v1/tasks/:id')     - get single task
+// app.patch('/api/v1/tasks/:id')   - update task
+// app.delete('/api/v1/tasks/:id')  - delete task
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    console.log(process.env.MONGO_URI);
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
