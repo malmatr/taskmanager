4a85696c7f160956a2973c64ca584f01a0c992c7
mallory matravers <matraversmallory@gmail.com>
2026-01-27 19:23:59 -0700

    update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 const connectDB = (url) => {
+  const dns = require("node:dns");
+  dns.setServers(["8.8.8.8", "8.8.4.4"]);
+  const servers = dns.getServers();
+  console.log("Node.js is using these DNS servers:", servers);
+
   return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
