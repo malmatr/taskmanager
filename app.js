69df10cbc1697f22a4e6ff28ba24c95084a73f68
mallory matravers <matraversmallory@gmail.com>
2026-01-27 17:45:48 -0700

    final commit

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const connectDB = require("./db/connect");
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
+
 app.use(express.static("./public"));
 app.use(express.json());
 
@@ -12,19 +15,16 @@ app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
 
-// app.get('/api//v1/tasks')        - get all the tasks
-// app.post('/api/v1/tasks')        - create a new task
-// app.get('/api/v1/tasks/:id')     - get single task
-// app.patch('/api/v1/tasks/:id')   - update task
-// app.delete('/api/v1/tasks/:id')  - delete task
-
-const port = 3000;
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    console.log(process.env.MONGO_URI);
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, console.log(`server is listening on port ${port}...`));
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`),
+    );
   } catch (error) {
     console.log(error);
   }
